f62a5ca19bdba5dcc8d68ea1029b56c72d2947ec
Pajojda_1 <vladgulin46@gmail.com>
2023-12-23 18:33:48 +0300

    alpha version Rewrote the HARD code according to the OOP principle

--- main.py
+++ main.py
@@ -2,56 +2,65 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_html(url: str) -> str:
+class Url:
+    
+    tasks = None
 
-    header = {
-        "Accept": "*/*",
-        "User-Agent": "Mozilla/5.0 (compatible; MSIE 10.0; Windows; Windows NT 6.1; WOW64; en-US Trident/6.0)"
-    }
+    def __init__(self, url):
+        self.url = url
+        self.header = {
+            "Accept": "*/*",
+            "User-Agent": "Mozilla/5.0 (compatible; MSIE 10.0; Windows; Windows NT 6.1; WOW64; en-US Trident/6.0)"
+        }
 
-    req = requests.get(url, headers=header)
-    return req.text
+        self.tasks = requests.get(self.url, headers=self.header).text
 
 
-def get_all_hrefs(task: str) -> list:
-    all_hrefs_and_title = []
+class Href(Url):
 
-    soup = BeautifulSoup(task, 'lxml')
+    def __int__(self):
+        self.all_hrefs_and_title = []
+        self.count_error = 0
 
-    sections = soup.find('div', class_="task-list").find_all('section', class_="active section-task")
+    def get_all_hrefs(self) -> list:
+        count_error = 0
+        soup = BeautifulSoup(self.tasks, 'lxml')
+        sections = soup.find('div', class_="task-list").find_all('section', class_="active section-task")
 
-    count_error = 0
+        all_hrefs = []
 
-    for section_task in sections:
-        for task in section_task.find_all("a"):
-            try:
-                if len(sections) == 1:
-                    all_hrefs_and_title.append({
-                        'title': f'Номер задания {task.get("title")}',
-                        'href': f'https://gdz.ru/{task.get("href")}'
-                    })
-                else:
-                    all_hrefs_and_title.append({
-                        'title': f'{section_task.find("h2", class_="heading").text.strip()} {task.get("title")}',
-                        'href': f'https://gdz.ru/{task.get("href")}'
-                    })
-            except AttributeError:
-                print("###################################")
-                count_error += 1
+        for section_task in sections:
+            for task in section_task.find_all("a"):
+                try:
+                    if len(sections) == 1:
+                        all_hrefs.append({
+                            'title': f'Номер задания {task.get("title")}',
+                            'href': f'https://gdz.ru/{task.get("href")}'
+                        })
+                    else:
+                        all_hrefs.append({
+                            'title': f'{section_task.find("h2", class_="heading").text.strip()} {task.get("title")}',
+                            'href': f'https://gdz.ru/{task.get("href")}'
+                        })
+                except AttributeError:
+                    count_error += 1
 
-    print(count_error)
-    return all_hrefs_and_title
+        print(count_error)
+        return all_hrefs
+
+    def show_all_hrefs(self):
+        self.get_all_hrefs()
+        for i in self.all_hrefs_and_title:
+            print(i)
 
 
 def start_parse() -> None:
 
     print("Привет, данный скрипт поможет тебе спарсить ВСЁ ГДЗ!))!))))!)00!))")
-    start_link = input("Пожалуйста, отправь мне ссылку на сайт, в котором изображены номера заданий из твоего учебнка!\n")
-    task = get_html(start_link)
+    start_link = input("Пожалуйста, отправь мне ссылку на сайт, в котором изображены номера заданий"
+                       " из твоего учебнка!\n")
 
-    all_hrefs = get_all_hrefs(task)
-    for i in all_hrefs:
-        print(i)
+    Href(start_link).show_all_hrefs()
 
 
 if __name__ == '__main__':
