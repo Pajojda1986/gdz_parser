e22c596e5d2ae784041ee7951d19013f009847ec
Pajojda_1 <vladgulin46@gmail.com>
2023-12-23 22:34:07 +0300

    first version add file scraping on the directory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from time import sleep
+from os import mkdir, chdir
 from bs4 import BeautifulSoup
 
 
@@ -32,20 +34,16 @@ class Href(Url):
         for section_task in sections:
             for task in section_task.find_all("a"):
                 try:
-                    if len(sections) == 1:
-                        all_hrefs.append({
-                            'title': f'Номер задания {task.get("title")}',
-                            'href': f'https://gdz.ru/{task.get("href")}'
-                        })
-                    else:
-                        all_hrefs.append({
-                            'title': f'{section_task.find("h2", class_="heading").text.strip()} {task.get("title")}',
-                            'href': f'https://gdz.ru/{task.get("href")}'
-                        })
+                    all_hrefs.append({
+                        'title': f'{section_task.find("h2", class_="heading").text.strip()}',
+                        'number': f' {task.get("title")}',
+                        'href': f'https://gdz.ru{task.get("href")}'
+                    })
                 except AttributeError:
                     count_error += 1
 
         print(count_error)
+        self.all_hrefs_and_title = all_hrefs
         return all_hrefs
 
     def show_all_hrefs(self):
@@ -54,13 +52,47 @@ class Href(Url):
             print(i)
 
 
+class ImageOnHref(Href):
+    def get_jpg(self, directory):
+
+        print("Image scraping start!")
+
+        all_hrefs = self.get_all_hrefs()
+        for img in all_hrefs:
+
+            number = requests.get(img['href'], headers=self.header).text
+            sleep(0.5)
+            soup = BeautifulSoup(number, 'lxml')
+
+            img_href = 'https:' + soup.find('div', class_="with-overtask").find('img').get('src')
+
+            dir = directory + '/' + BeautifulSoup(self.tasks, 'lxml').find('title').next_element.text
+
+            try:
+                mkdir(dir)
+                chdir(dir)
+
+            except FileExistsError:
+                chdir(dir)
+
+            chdir(dir)
+            p = requests.get(img_href)
+            out = open(f"{img['number']}.jpg", "wb")
+            out.write(p.content)
+            out.close()
+            chdir(directory)
+            print(f"Successfully {img['number']}")
+
+
 def start_parse() -> None:
 
-    print("Привет, данный скрипт поможет тебе спарсить ВСЁ ГДЗ!))!))))!)00!))")
-    start_link = input("Пожалуйста, отправь мне ссылку на сайт, в котором изображены номера заданий"
-                       " из твоего учебнка!\n")
+    print("Привет, данный скрипт поможет тебе спарсить все картинки с гдз с сайта https://gdz.ru/")
+    start_link = input("Пожалуйста, отправь мне ссылку на страницу, на который изображены все"
+                       " номера заданий из выбранного учебника!\n")
 
-    Href(start_link).show_all_hrefs()
+    current_dir = input("Выберете директорию (папку) для фото!\n")
+
+    ImageOnHref(start_link).get_jpg(current_dir)
 
 
 if __name__ == '__main__':
